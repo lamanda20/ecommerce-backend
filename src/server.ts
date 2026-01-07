94a690349270bc83c4a0707ecf4fd95167637946
unknown <tahaghadi3@gmail.com>
2026-01-07 01:58:56 +0100

    Fix tsconfig rootDir + prod build2

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
 import express from "express";
 import cors from "cors";
-import productsRoutes from "./routes/products";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (_req, res) => {
-    res.json({ status: "API running" });
+app.get("/", (req, res) => {
+    res.send("Backend is running 🚀");
 });
-
-app.use("/products", productsRoutes);
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
-
